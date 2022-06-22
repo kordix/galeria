a0e000366bb9c2a4a9fe7f2c6a2b0946d57b617a
Kordian Bober <BOBKOR@ad.bertrand.pl>
2022-06-22 12:53:13 +0200

    converter

---
+++ api/converter.php
@@ -0,0 +1,28 @@
+<?php
+// Create image instances
+
+@$folder = $_GET['folder'];
+
+@$image = $_GET['image'];
+
+$targetimagestring = '../uploads/'.$folder.'/'.$image.'.jpg';
+
+$src = imagecreatefromjpeg($targetimagestring);
+
+// Copy
+$image_info = getimagesize($targetimagestring);
+$width = $image_info[0] / 3;
+$height = $image_info[1] / 3;
+
+echo $width;
+
+
+$src = imagescale($src, $width,$height);
+
+// Output and free from memory
+//header('Content-Type: image/jpg');
+imagejpeg($src,'../uploads/'.$folder.'/'.$image.'_T.jpg');
+
+// imagedestroy($dest);
+// imagedestroy($src);
+?>
--- api/upload.php
+++ api/upload.php
@@ -14,6 +14,16 @@
       <a href="/" style="margin-right:50px;font-size:20px;text-decoration:none;font-family:arial">Main page</a>
       <br><br>
       <form action="/api/upload.php" method="post" enctype="multipart/form-data">
+    
+        <label for="">Folder</label>
+        <select name="folder">
+          <option value="journeys">Podróże</option>
+          <option value="inne">INNE</option>
+        </select>
+
+        <label for="">Nazwa pliku</label>
+        <input type="text" name="filename">
+
         Dodaj plik:
         <input type="file" name="fileToUpload" id="fileToUpload">
         <input type="submit" value="Upload Image" name="submit">
@@ -29,36 +39,44 @@
 
   include('dir.php');
 
-  $folder = $_GET['folder'];
+  @$folder = $_POST['folder'];
 
-  $target_file = $target_dir . $folder . $ukosnik. basename($_FILES["fileToUpload"]["name"]);
-  $uploadOk = 1;
-  $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+ echo @$folder;
+
+if (count($_FILES) == 0) {
+    return;
+}
+
+   $target_file = $target_dir . $folder . '/'. basename($_FILES["fileToUpload"]["name"]);
+   
+   $uploadOk = 1;
+   $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+   $target_file = $target_dir . $folder . '/'. basename($_POST['filename'].'.'.$imageFileType);
 
 
 
   // Check if image file is a actual image or fake image
   if (isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if ($check !== false) {
-      echo "File is an image - " . $check["mime"] . ".";
-      $uploadOk = 1;
-    } else {
-      // echo "File is not an image.";
-      $uploadOk = 1;
-    }
+      $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+      if ($check !== false) {
+          echo "File is an image - " . $check["mime"] . ".";
+          $uploadOk = 1;
+      } else {
+          // echo "File is not an image.";
+          $uploadOk = 1;
+      }
   }
 
   // Check if file already exists
   if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
+      echo "Sorry, file already exists.";
+      $uploadOk = 0;
   }
 
   // Check file size
   if ($_FILES["fileToUpload"]["size"] > 7000000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
+      echo "Sorry, your file is too large.";
+      $uploadOk = 0;
   }
 
   // Allow certain file formats
@@ -70,14 +88,14 @@
 
   // Check if $uploadOk is set to 0 by an error
   if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
+      echo "Sorry, your file was not uploaded.";
+  // if everything is ok, try to upload file
   } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-      echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
-    } else {
-      echo "Sorry, there was an error uploading your file.";
-    }
+      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+          echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+      } else {
+          echo "Sorry, there was an error uploading your file.";
+      }
   }
 
   ?>
